7f7ff0d551c7e62b93a52563670b37a6f39ebf64
Oswaldo <orubio@mobimento.com>
2015-05-12 12:25:45 +0200

    Initial Commit

---
+++ VerticalCenteredScrollViewExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VerticalCenteredScrollViewExample
+//
+//  Created by oswaldo on 12/05/15.
+//  Copyright (c) 2015 osrufung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VerticalCenteredScrollViewExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VerticalCenteredScrollViewExample
+//
+//  Created by oswaldo on 12/05/15.
+//  Copyright (c) 2015 osrufung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
